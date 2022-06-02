8712885ff70d40c9576c372f614228231f1bda62
Puneet Dabral <puneetdabral2000@gmail.com>
2022-06-02 11:47:53 +0530

    learn take input in run time and use it as a number

---
+++ input.py
@@ -0,0 +1,10 @@
+# name = input('wht is your name :')
+# age= input('age is  :')
+# print(name,'you are',age)
+
+# area of circle
+radius =input('enter the redious :')
+# in input all the things is string it give error so we can use typr casting 
+area =  3.14*int(radius)**2
+
+print(area)
