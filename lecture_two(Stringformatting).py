40f8c295087012dc03adb613e51055c7fca333ed
Puneet Dabral <puneetdabral2000@gmail.com>
2022-06-04 18:36:56 +0530

    string formatting

---
+++ lecture_two(Stringformatting).py
@@ -0,0 +1,16 @@
+a = 3.12345
+b= 5.12345
+
+
+# print('value of a :',a ,'value of b :',b)
+
+
+# forat method
+# print('a is {0} and b is {1}'.format(a,b))
+# print('a is {0:.3} and b is {1}'.format(a,b))
+# print('a is {0:.3f} and b is {1}'.format(a,b))
+
+
+# //using f  strings
+print(f'a is {a} and b is {b}')
+# print(f'a is {a:.3} and b is {b}')
