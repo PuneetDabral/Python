082569fe48fbaed32e69e9de9aa9ef60b31d234f
Puneet Dabral <puneetdabral2000@gmail.com>
2022-06-05 15:51:52 +0530

    while loo[

---
+++ lecture_5_whileloop.py
@@ -0,0 +1,15 @@
+# WHILE LOOP STATEMENT
+age = 25
+num = 0
+
+while num < age:
+    if num == 0:
+        num += 1
+        continue
+    if num % 2 == 0:
+        print(num)
+    num += 1
+    if num > 10:
+        break
+else:
+    print('outside of condition') #doesn't work when using break
